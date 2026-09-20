a5d6bb6e5ead4b1b2739476dc5207514b92b6524
ExoticSecurityWeb <pinkparadiiise@gmail.com>
2026-09-20 18:41:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,15 +6,16 @@ mkdir -p /config /backup
 
 echo "=== Home Assistant B2 ==="
 
-# Restaurer uniquement si l'installation est vide.
+# Restaurer la configuration depuis B2
 python3 /backup_b2.py restore || true
 
-# Configuration Render utilisée par Home Assistant.
-if [ ! -f /config/configuration.yaml ] && [ -f /etc/secrets/configuration.yaml ]; then
+# Toujours remettre la configuration Render
+# afin de conserver le port 10000.
+if [ -f /etc/secrets/configuration.yaml ]; then
     cp /etc/secrets/configuration.yaml /config/configuration.yaml
 fi
 
-# Backup automatique B2.
+# Lancer la surveillance des backups
 python3 /backup_b2.py watch &
 
 echo "Starting Home Assistant..."
